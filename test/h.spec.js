1b02c564e1c4655f538d5a388a022356a636c79f
Mike Wilcox <mwilcox56@gmail.com>
2017-11-13 20:02:34 -0500

    get tests setup

---
+++ test/h.spec.js
@@ -0,0 +1,10 @@
+import expect from 'expect';
+import { h } from '../src';
+
+describe('h.js', () => {
+  it('should return the type of the node', () => {
+    ['p', 'div', 'span'].forEach(tag => {
+      expect(h(tag, null, null).type).toEqual(tag);
+    });
+  });
+});
