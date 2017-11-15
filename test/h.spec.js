771c966c7b9296dff50f7f5208352af5f7d58b81
Mike Wilcox <mwilcox56@gmail.com>
2017-11-14 20:45:53 -0500

    update spec

--- test/h.spec.js
+++ test/h.spec.js
@@ -4,7 +4,7 @@ import { h } from '../src';
 describe('h.js', () => {
   it('should return the type of the node', () => {
     ['p', 'div', 'span'].forEach(tag => {
-      expect(h(tag, null, null).type).toEqual(tag);
+      expect(h(tag, null, null).nodeName).toEqual(tag);
     });
   });
 });
